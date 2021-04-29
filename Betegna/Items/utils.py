557bd1290260a1826ff42feb738867a9486ad2f1
chapimenge3 <chapimenge3@gmail.com>
2021-04-29 17:35:22 -0400

    bug fix

--- Betegna/Items/utils.py
+++ Betegna/Items/utils.py
@@ -4,4 +4,5 @@ import uuid
 def item_directory_path(instance, filename):
     ext = (filename).split(".")[-1]
     filename = str(uuid.uuid1())
+    print(instance, instance.pk)
     return f"store/{instance.id}-{filename}.{ext}"
