014431628dcaf389f54d7087cbe168b3922a4555
chapimenge3 <chapimenge3@gmail.com>
2021-04-29 14:39:55 -0400

    Static File configration in setting.s.py

--- Betegna/Betegna/settings.py
+++ Betegna/Betegna/settings.py
@@ -116,10 +116,16 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
-
+ 
 STATIC_URL = '/static/'
+STATIC_URL = "/static/"
+STATICFILES_DIRS = [
+    BASE_DIR / "static"
+]
+STATIC_ROOT = BASE_DIR / 'static_root'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
